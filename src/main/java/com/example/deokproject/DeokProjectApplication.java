4cd7e881e3faafa78b2cbc62d6be69f7e86a5cb0
duckyear <kdn2869@naver.com>
2025-06-28 19:53:48 +0900

    css 수정

---
+++ src/main/java/com/example/deokproject/DeokProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.deokproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeokProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeokProjectApplication.class, args);
+    }
+
+}
